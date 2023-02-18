f75e15f4e4d68d3c052c7af608009a9b1de74a9f
Zou Minowa <zou.minowa@shoppermediagroup.com>
2023-02-18 20:41:57 +0000

    add some test code

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-process.on('exit', () => console.log('hello world'));
+process.on('exit', () => console.log('goodbye world'));
 
-console.log(process);
+let count = 0;
+
+function executeFirst() {
+    setImmediate(() => {
+        import('./executeLater.js');
+        import('./executeLater.js');
+    });
+    setImmediate(() => {
+        import('./executeEvenLater.js');
+    });
+
+    return () => console.log(`hello world ${count++}`);
+}
+
+export default executeFirst();
